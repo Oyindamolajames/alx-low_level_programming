7d0b2ab8d613eecd42555c882a64f4e1a26eacbd
Ogunsanya James <oyindamolajames@gmail.com>
2022-12-11 12:16:17 -0800

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,14 +16,14 @@ int main(void)
 			{
 				putchar((a / 10) + 48);
 				putchar((a % 10) + 48);
-				putchar('');
+				putchar(' ');
 				putchar((b / 10) + 48);
 				putchar((b % 10) + 48);
 				if (a != 98 || b != 99)
 				{
 					putchar(',');
 
-					putchar('');
+					putchar(' ');
 				}
 			}
 		}

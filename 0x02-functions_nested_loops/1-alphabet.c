e76f5c0298f8bd8e2caaefb1ce2ed9b4242bea34
Ogunsanya James <oyindamolajames@gmail.com>
2022-12-13 12:38:41 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in small letter a - z
+ */
+
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+	_putchar('\n')
+}

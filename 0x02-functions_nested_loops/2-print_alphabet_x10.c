f7c00c9f9aecb5a380cc4a0cdbd96057c79481fb
Ogunsanya James <oyindamolajames@gmail.com>
2022-12-13 12:59:10 -0800

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints alphabets in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+	char lc;
+
+	for (num = 0; num <= 10; num++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+			_putchar(lc);
+		_putchar('\n');
+	}
+}

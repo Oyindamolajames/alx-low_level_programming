9ed453d14feb9f1b893b41e309534ee619ed52fe
Ogunsanya James <oyindamolajames@gmail.com>
2023-01-07 10:37:26 -0800

    libmy

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -8,11 +8,11 @@
 
 void _puts(char *str)
 {
-        int a;
+	int a;
 
-        for (a = 0; str[a] != '\0'; a++)
-        {
-                _putchar(str[a]);
-        }
-        _putchar('\n');
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
 }

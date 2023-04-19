5c509c0c4c4b7fd56caa89cc91b8efe8658ab3ff
Oyindamolajames <oyindamolajames@gmail.com>
2023-04-19 08:57:59 -0700

    modifed dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,14 @@
  * Description: This struct represents a dog with a name, age, and owner
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dogt;
+};
 
 /* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif

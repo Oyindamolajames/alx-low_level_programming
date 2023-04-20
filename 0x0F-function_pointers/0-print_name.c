60546fda3b204fc8d2fece65150b982d3cc070d4
Oyindamolajames <oyindamolajames@gmail.com>
2023-04-20 04:56:22 -0700

    print name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+/** 
+ * print_name - A function to print a name
+ * @name: A pointer to name to print
+ * @f: a pointer to function that prints name
+ *
+ * Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+	f(name);
+	}
+}
